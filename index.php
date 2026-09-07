142232746f66e7bdb94236d9638b9af90daf0220
ShaneDelaRama <shane622005@gmail.com>
2026-09-07 14:01:17 +0800

    added wishlist

--- index.php
+++ index.php
@@ -1,18 +1,33 @@
 <?php
 
-session_start(); 
+session_start();
 
+//TODOS
 if(!isset($_SESSION["todos"])){
     $_SESSION["todos"] = [];//makes an empty array for todos. Need to initialize this first!
 }
 
 if (isset($_POST["todo"])){ 
-    if(!empty($todo)){
-                            //okay we need to add PRG (post, redirect, get). Bug found adds last added word repeatedly
+    if(!empty($todo)){ //checks for no empty strings
         $todo = $_POST["todo"];
         $_SESSION["todos"][] = $todo;//stores it in session todos
         header("Location: index.php");
         exit;
+    }
+    else{
+        echo ("<p>Nothing entered, please try again.</p>");
+    }
+}
+//WISHLIST
+if (!isset($_SESSION["wish"])){
+    $_SESSION["wishes"]= [];
+}
+
+if (isset($_POST["wish"])){
+    if(!empty($wish)){
+        $wish = $_POST["wish"];
+        $_POST["wish"] = $_SESSION["wishes"];
+        header("location: index.php");
     }
     else{
         echo ("<p>Nothing entered, please try again.</p>");
@@ -28,11 +43,21 @@ if (isset($_POST["todo"])){
     <button type ="submit">Add Task</button>
 </form>
 
+<form method = "POST"> 
+    <input type = "text" name="wish">
+    <button type ="submit">Add Wish</button>
+</form>
 
+<ul>
 <?php
 foreach ( $_SESSION["todos"] as $todo){ //do your request-processing logic before producing your page.
-    echo "<ul>";
-    echo ("<li>$todo<li>");
-    echo "<ul>";
+    
+    echo "<li>$todo</li>";
+}
+
+foreach ( $_SESSION["wishes"] as $wish){
+    
+    echo "<li>$wish</li>";
 }
 ?>
+</ul>
